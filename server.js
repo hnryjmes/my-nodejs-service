ec117b6f13a444fea9b07d7de2ce557564ce6b15
Henry Cooksley <hcooksley@deloitte.co.uk>
2019-09-10 14:25:23 +0100

    Add view and Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const path = require(`path`);
+
 app.get('/', (req, res) => {
   res.send('Hello from App Engine!');
 });
@@ -9,4 +11,8 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
+});
+
+app.get('/submit', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/form.html'));
 });
