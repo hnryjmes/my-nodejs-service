5f3ef1e11b4560405d790609ffaeaa1770c72988
Henry Cooksley <hcooksley@deloitte.co.uk>
2019-09-10 14:51:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 
 const path = require(`path`);
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello from App Engine!');
@@ -15,4 +18,12 @@ app.listen(PORT, () => {
 
 app.get('/submit', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/form.html'));
+});
+
+app.post('/submit', (req, res) => {
+  console.log({
+    name: req.body.name,
+    message: req.body.message
+  });
+  res.send('Thanks for your message!');
 });
